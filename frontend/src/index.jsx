f7ec9e41843dfea5c5f342fe3f24685d30066c2a
Stephen Szpak <srszpak@gmail.com>
2025-06-24 11:54:26 -0500

    Add WeatherProvider and show time/date/temp in navbar

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
 import App from './App';
+
+const theme = createTheme({
+  palette: { mode: 'dark' },
+  typography: { fontFamily: 'Verdana, sans-serif' }
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
